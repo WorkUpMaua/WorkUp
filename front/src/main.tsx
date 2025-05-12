2fa08e5253ebfee0bf376497184690d924e6face
Lucas Galhardo <lucalhardo@gmail.com>
2025-05-12 00:37:51 -0300

    feat: adicionando a página workspace e atualizando a rota

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/index.css'
 import Catalog from './pages/Catalog.tsx'
+import Workspace from './pages/Workspace.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="catalog" element={<Catalog />} />
+        <Route path="workspace" element={<Workspace />} />
       </Routes>
     </Router>
   </StrictMode>,
