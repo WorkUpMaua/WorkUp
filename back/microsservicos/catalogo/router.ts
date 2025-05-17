a4780d5848bbeff968f8d6af90b84ae43a3dfea1
Victor Gasperi <victorgasperi@gmail.com>
2025-05-16 23:57:56 -0300

    feat(catalogo-router): rotas novas adicionadas

--- back/microsservicos/catalogo/router.ts
+++ back/microsservicos/catalogo/router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getHelloWorldController } from "./app/getHelloWorld/getHelloWorldController";
+import { getAllCatalogoController } from "./app/get_all_catalogo/get_all_catalogo_presenter";
+import { CatalogoRepositoryMock } from "./repo/catalogoRepositoryMock";
+import { createCatalogoController } from "./app/create_catalogo/create_catalogo_presenter";
 
 const router: Router = Router()
 
-router.get('/', (req, res) => getHelloWorldController.handle(req, res))
+router.get('/catalogo', (req, res) => getAllCatalogoController.handle(req, res))
+router.post('/catalogo', (req, res) => createCatalogoController.handle(req, res))
 
-export { router }
+export { router,  }
