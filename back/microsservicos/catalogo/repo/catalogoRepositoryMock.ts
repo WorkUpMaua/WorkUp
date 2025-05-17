f11913b2be69f83d7ee15658a5626686fea6f76e
Victor Gasperi <victorgasperi@gmail.com>
2025-05-16 23:51:09 -0300

    feat(repository): adicionado o repositorio do catalogo

---
+++ back/microsservicos/catalogo/repo/catalogoRepositoryMock.ts
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export type CatalogoType = {
+    id: string
+    name: string
+    description: string
+    address: string
+    comodities: string[]
+    pictures: string[]
+    price: number
+}
+
+type baseCatalogoType = { 
+    [key: string]: CatalogoType 
+}
+export class CatalogoRepositoryMock {
+
+    private baseCatalogo: baseCatalogoType = {}
+
+    public getAllCatalogo(): baseCatalogoType {
+        return this.baseCatalogo
+    }
+
+    public createCatalogo(props: CatalogoType): CatalogoType {
+
+        const id = uuidv4()
+
+        const room: CatalogoType = {
+            id,
+            name: props.name,
+            description: props.description,
+            address: props.address,
+            comodities: props.comodities,
+            pictures: props.pictures,
+            price: props.price
+        }
+
+        return room
+    }
+
+}
