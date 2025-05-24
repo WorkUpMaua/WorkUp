0d4a7498d4795947bab25e8aecc799ea6bcdcd83
Victor Gasperi <victorgasperi@gmail.com>
2025-05-23 23:41:21 -0300

    refactor(userRepositoryMock): valida se o usuario ja existe

--- back/microsservicos/user/repo/userRepositoryMock.ts
+++ back/microsservicos/user/repo/userRepositoryMock.ts
@@ -1,78 +1,83 @@
-import { v4 as uuidv4 } from 'uuid';
-import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
 
 export type informationType = {
-    id: string
-    name: string
-    email: string
-    cpf: string
-    birth: number
-    phone: string
-}
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  birth: number;
+  phone: string;
+};
 
 export type authType = {
-    username: string
-    password: string
-}
+  username: string;
+  password: string;
+};
 
 export interface User {
-    auth: authType
-    information: informationType
+  auth: authType;
+  information: informationType;
 }
 
 // Deixa o id opcional e o resto obrigatorio
-export type informationWithoutID = Omit<informationType, 'id'> & Partial<Pick<informationType, 'id'>>
+export type informationWithoutID = Omit<informationType, "id"> &
+  Partial<Pick<informationType, "id">>;
 
 export type createUserPropsType = {
-    auth: authType
-    information: informationWithoutID
-}
+  auth: authType;
+  information: informationWithoutID;
+};
 
 type baseUserType = {
-    [key: string]: User
-}
+  [key: string]: User;
+};
 
 export class UserRepositoryMock {
+  private baseUser: baseUserType = {};
 
-    private baseUser: baseUserType = {}
+  private encryptPass(pass: string): string {
+    const saltRounds = 8;
+    const hash = bcrypt.hashSync(pass, saltRounds);
+    return hash;
+  }
 
-    private encryptPass(pass: string): string {
-        const saltRounds = 8
-        const hash = bcrypt.hashSync(pass, saltRounds)
-        return hash
-    }
+  private checkDuplicateUser(username: string): boolean {
+    return Object.values(this.baseUser).some(
+      (user) => user.auth.username === username
+    );
+  }
 
-    public getAllUser(): baseUserType {
-        return this.baseUser
-    }
+  public getAllUser(): baseUserType {
+    return this.baseUser;
+  }
 
-    public createUser(props: createUserPropsType): informationType {
+  public createUser(props: createUserPropsType): informationType {
+    const id = uuidv4();
 
-        const id = uuidv4()
+    if(this.checkDuplicateUser(props.auth.username)) throw new Error('User already exists')
 
-        const auth: authType = {
-            username: props.auth.username,
-            password: this.encryptPass(props.auth.password)
-        } 
+    const auth: authType = {
+      username: props.auth.username,
+      password: this.encryptPass(props.auth.password),
+    };
 
-        const information: informationType = {
-            id,
-            name: props.information.name,
-            email: props.information.email,
-            cpf: props.information.cpf,
-            birth: props.information.birth,
-            phone: props.information.phone
-        }
+    const information: informationType = {
+      id,
+      name: props.information.name,
+      email: props.information.email,
+      cpf: props.information.cpf,
+      birth: props.information.birth,
+      phone: props.information.phone,
+    };
 
-        const user: User = {
-            auth,
-            information
-        } 
+    const user: User = {
+      auth,
+      information,
+    };
 
-        this.baseUser[id] = user
+    this.baseUser[id] = user;
 
-        return information
-
-    }
-
+    return information;
+  }
 }
