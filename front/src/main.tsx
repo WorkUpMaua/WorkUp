4dc4d002fd05d8619d4de7c19cb77c9e21a84af8
João Paulo Bonagurio Ramirez <jpbr2004@gmail.com>
2025-05-25 20:28:14 -0300

    feat(routes): atualizar redirecionamento padrão para página de cadastro

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,14 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './styles/index.css'
 import Catalog from './pages/Catalog.tsx'
+import Cadastro from './pages/Cadastro.tsx'
 import Workspace from './pages/Workspace.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/catalog" replace />} />
+        <Route path="/" element={<Navigate to="/cadastro" replace />} />
         <Route path="catalog" element={<Catalog />} />
+        <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/workspace/:officeId" element={<Workspace />} />
       </Routes>
     </Router>
