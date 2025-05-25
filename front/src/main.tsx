bf0ee13b77b7210574e7b513bd1620821345359f
João Paulo Bonagurio Ramirez <jpbr2004@gmail.com>
2025-05-25 20:14:00 -0300

    feat(routes): atualização do router para redirecionar para a Login page

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import './styles/index.css'
 import Catalog from './pages/Catalog.tsx'
 import Workspace from './pages/Workspace.tsx'
+import Login from './pages/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/catalog" replace />} />
+        <Route path="/" element={<Navigate to="/Login" replace />} />
         <Route path="catalog" element={<Catalog />} />
+        <Route path="login" element={<Login />} />
         <Route path="/workspace/:officeId" element={<Workspace />} />
       </Routes>
     </Router>
