c97e817ac079134fc9512c3fa0f6d99c8e481bc7
Victor Gasperi <victorgasperi@gmail.com>
2025-05-27 00:53:20 -0300

    fix: remove Catalog route and adjust home route

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Catalog from './pages/Catalog'
 import Cadastro from './pages/Cadastro'
 import Workspace from './pages/Workspace'
 import Login from './pages/Login'
@@ -18,7 +17,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/workspace/:id" element={<Workspace />} />
         <Route path="/user-profile" element={<UserProfile />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="catalog" element={<Catalog />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   </StrictMode>
