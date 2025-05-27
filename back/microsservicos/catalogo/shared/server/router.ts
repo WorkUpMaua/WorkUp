3042c83f26caab8e80e7ee0187615de23d9f5b5e
Victor Gasperi <victorgasperi@gmail.com>
2025-05-26 23:39:00 -0300

    fix: adicionar async ao manipulador de criação de catálogo

--- back/microsservicos/catalogo/shared/server/router.ts
+++ back/microsservicos/catalogo/shared/server/router.ts
@@ -9,7 +9,7 @@ const router: Router = Router()
 
 router.get('/catalogo', (req, res) => getAllCatalogoController.handle(req, res))
 router.get('/catalogo/:id', (req, res) => getCatalogoController.handle(req, res))
-router.post('/catalogo', (req, res) => createCatalogoController.handle(req, res))
+router.post('/catalogo', async (req, res) => await createCatalogoController.handle(req, res))
 router.delete('/catalogo/:id', (req, res) => deleteCatalogoController.handle(req, res))
 router.patch('/catalogo/:id', (req, res) => updateCatalogoController.handle(req, res))
 
