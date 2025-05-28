1a72d4c9ecb29bb179d1d79f007c846d9b1d6f1a
Lucas Galhardo <lucalhardo@gmail.com>
2025-05-28 03:35:59 -0300

    feat(aluguel): rota de criar alguel de workspace

--- back/microsservicos/aluguel/environments.ts
+++ back/microsservicos/aluguel/environments.ts
@@ -1,11 +1,17 @@
 import { AluguelRepositoryMock } from './shared/repo/aluguelRepositoryMock';
+import { CatalogoRepositoryMock } from './shared/repo/catalogoRepositoryMock';
+import { UserRepositoryMock } from './shared/repo/userRepositoryMock';
 
 export class Environments {
   private static _instance: Environments;
-  private _repo: AluguelRepositoryMock;
+  private _repoAluguel: AluguelRepositoryMock;
+  private _repoUser: UserRepositoryMock;
+  private _repoCatalogo: CatalogoRepositoryMock;
 
   private constructor() {
-    this._repo = new AluguelRepositoryMock();
+    this._repoAluguel = new AluguelRepositoryMock();
+    this._repoUser = new UserRepositoryMock();
+    this._repoCatalogo = new CatalogoRepositoryMock();
   }
 
   public static get instance(): Environments {
@@ -15,7 +21,15 @@ export class Environments {
     return this._instance;
   }
 
-  public get repo(): AluguelRepositoryMock {
-    return this._repo;
+  public get repoAluguel(): AluguelRepositoryMock {
+    return this._repoAluguel;
+  }
+
+  public get repoUser(): UserRepositoryMock {
+    return this._repoUser;
+  }
+
+  public get repoCatalogo(): CatalogoRepositoryMock {
+    return this._repoCatalogo;
   }
 }
