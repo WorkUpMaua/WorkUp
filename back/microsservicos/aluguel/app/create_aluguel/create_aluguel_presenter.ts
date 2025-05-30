1b83f9b3f80732f03943bcba9ed5eba6ecdc60bc
Victor Gasperi <victorgasperi@gmail.com>
2025-05-30 01:13:10 -0300

    Remove aluguel microservice and its related files

--- back/microsservicos/aluguel/app/create_aluguel/create_aluguel_presenter.ts
+++
@@ -1,9 +0,0 @@
-import { Environments } from "../../environments";
-import { CreateAluguelController } from "./create_aluguel_controller";
-import { CreateAluguelUsecase } from "./create_aluguel_usecase";
-
-const repoAluguel = Environments.instance.repoAluguel
-const repoUser = Environments.instance.repoUser
-const repoCatalogo = Environments.instance.repoCatalogo
-const usecase = new CreateAluguelUsecase(repoAluguel, repoUser, repoCatalogo)
-export const createAluguelController = new CreateAluguelController(usecase)
