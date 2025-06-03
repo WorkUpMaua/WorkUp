2ac4d8e8cbc8f92616e06595ef8adbbd8fe1857c
Lucas Galhardo <lucalhardo@gmail.com>
2025-06-03 00:04:20 -0300

    fix: voltando as configurações como eram

--- back/microsservicos/aluguel/environments.ts
+++ back/microsservicos/aluguel/environments.ts
@@ -1,17 +1,11 @@
-import { AluguelRepositoryMock } from './shared/repo/aluguelRepositoryMock';
-import { CatalogoRepositoryMock } from './shared/repo/catalogoRepositoryMock';
-import { UserRepositoryMock } from './shared/repo/userRepositoryMock';
+import { AluguelRepositoryMock } from './shared/repo/aluguelRepositoryMock'
 
 export class Environments {
   private static _instance: Environments;
   private _repoAluguel: AluguelRepositoryMock;
-  private _repoUser: UserRepositoryMock;
-  private _repoCatalogo: CatalogoRepositoryMock;
 
   private constructor() {
     this._repoAluguel = new AluguelRepositoryMock();
-    this._repoUser = new UserRepositoryMock();
-    this._repoCatalogo = new CatalogoRepositoryMock();
   }
 
   public static get instance(): Environments {
@@ -23,13 +17,5 @@ export class Environments {
 
   public get repoAluguel(): AluguelRepositoryMock {
     return this._repoAluguel;
-  }
-
-  public get repoUser(): UserRepositoryMock {
-    return this._repoUser;
-  }
-
-  public get repoCatalogo(): CatalogoRepositoryMock {
-    return this._repoCatalogo;
   }
 }
--- back/microsservicos/aluguel/router.ts
+++ back/microsservicos/aluguel/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-// import { createAluguelController } from "./app/create_aluguel/create_aluguel_presenter";
+import { createAluguelController } from "./app/create_aluguel/create_aluguel_presenter";
 // import { getAllAluguelController } from "./app/get_all_aluguel/get_all_aluguel_presenter";
 // import { getAluguelController } from "./app/get_aluguel/get_aluguel_presenter";
 // import { updateAluguelController } from "./app/update_aluguel/update_aluguel_presenter";
@@ -8,7 +8,7 @@ import { Router } from "express";
 
 const router: Router = Router()
 
-// router.post('/aluguel', (req, res) => createAluguelController.handle(req, res))
+router.post('/aluguel', (req, res) => createAluguelController.handle(req, res))
 // router.get('/aluguel', (req, res) => getAllAluguelController.handle(req, res))
 // router.get('/aluguel/:id', (req, res) => getAluguelController.handle(req, res))
 // router.patch('/aluguel/:id', (req, res) => updateAluguelController.handle(req, res))
