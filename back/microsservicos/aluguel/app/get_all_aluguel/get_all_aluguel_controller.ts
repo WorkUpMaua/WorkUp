14277d2ab7527bdf3fe63651d4157db4a8629728
Lucas Galhardo <lucalhardo@gmail.com>
2025-06-03 00:28:22 -0300

    feat(aluguel): fazendo a rota de pegar todos os alugueis

---
+++ back/microsservicos/aluguel/app/get_all_aluguel/get_all_aluguel_controller.ts
@@ -0,0 +1,27 @@
+import { GetAllAluguelUsecase } from "./get_all_aluguel_usecase";
+import { Request, Response } from 'express'
+
+export class GetAllAluguelController {
+
+    constructor(private usecase: GetAllAluguelUsecase) {}
+
+    public handle(req: Request, res: Response): void {
+
+        try {
+
+            const rooms = this.usecase.execute()
+
+            res.status(200).json({
+                rooms,
+                "message": "Os alugueis foram recuperados"
+            });
+
+        } catch (err) {
+            res.status(500).json({
+                "message": err
+            })
+        }
+
+    }
+
+}
