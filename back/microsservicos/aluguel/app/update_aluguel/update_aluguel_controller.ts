6a68a5c4f1c60a9b9fece62535da1ee128356c92
Lucas Galhardo <lucalhardo@gmail.com>
2025-06-03 00:37:10 -0300

    feat(aluguel): fazendo a rota de atualizar aluguel

---
+++ back/microsservicos/aluguel/app/update_aluguel/update_aluguel_controller.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { Request, Response } from 'express'
+import { UpdateAluguelUsecase } from "./update_aluguel_usecase";
+import { updateAluguelProps } from "../../shared/types";
+
+export class UpdateAluguelController {
+
+    constructor(private usecase: UpdateAluguelUsecase) {}
+
+    public handle(req: Request, res: Response): void {
+
+        try {
+
+            const id = req.params.id;
+
+            if (id === undefined) throw new Error('ID não informado')
+            if (id.length !== 36) throw new Error('ID inválido')
+
+            const props: updateAluguelProps = { id, ...req.body };
+
+            const room_updated = this.usecase.execute(props)
+
+            // manda para o barramento de eventos
+            axios.post('http://localhost:10001/events', {
+                type: 'AluguelUpdated',
+                payload: props
+            })
+            .then()
+            .catch( (err) => console.log(err))
+            .finally( () => res.json({
+                "room": room_updated,
+                "message": "A reserva foi atualizada com sucesso!"
+            }) )
+
+        } catch (err) {
+            res.status(500).json({
+                message: err instanceof Error ? err.message : String(err)
+            })
+        }
+
+    }
+
+}
