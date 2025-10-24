3d042a045f03671e29265d5c6631a9ede3a02720
tiagooazevedo <tiagotadeu999@gmail.com>
2025-10-24 17:28:23 -0300

    feat: novo arquivo para testes de visualização

---
+++ mobile/lib/dev_visualization/dev_visual.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../screens/workspace.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WorkSpacePage (),
+    ),
+  );
+}
